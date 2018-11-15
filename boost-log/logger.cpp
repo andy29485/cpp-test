c8e28a5b289e9906b55eb02feb6852c0928544c3
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2018-11-15 09:46:17 -0500

    improved logging

--- boost-log/logger.cpp
+++ boost-log/logger.cpp
@@ -1,13 +1,24 @@
 #include <boost/log/trivial.hpp>
 #include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/utility/setup/file.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/expressions.hpp>
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", severity)
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
 
 void init() {
   boost::log::add_file_log(
     boost::log::keywords::file_name = "sample_%N.log",
     boost::log::keywords::rotation_size = 10 * 1024 * 1024,
     boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),
-    boost::log::keywords::format = "[%TimeStamp%]: %Message%"
+    boost::log::keywords::format = (
+      boost::log::expressions::stream
+                << boost::log::expressions::format_date_time< boost::posix_time::ptime >("TimeStamp", "%Y-%m-%d %H:%M:%S")
+                << ": <" << boost::log::trivial::severity
+                << "> " << boost::log::expressions::smessage
+    )
   );
 
   boost::log::core::get()->set_filter(
@@ -17,6 +28,9 @@ void init() {
 
 
 int main(int, char*[]) {
+  init();
+  boost::log::add_common_attributes();
+
   BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
   BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
   BOOST_LOG_TRIVIAL(info) << "An informational severity message";
